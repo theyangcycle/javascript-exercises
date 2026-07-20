2e5fbe534b657d84d4f3f39f4c24d28bd7797eb1
Jonah Yang <jonahyang2413@gmail.com>
2026-07-20 16:38:28 -0500

    complete 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...nums) {
+    for (let i = 0; i < arr.length; i++) {
+        if (nums.includes(arr[i])) {
+            arr.splice(i,1)
+            i--
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
