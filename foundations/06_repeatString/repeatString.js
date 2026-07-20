39d3213b2ccde0aa2d1973d2a9772250197d3650
Jonah Yang <jonahyang2413@gmail.com>
2026-07-20 16:05:51 -0500

    complete 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    newstr = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++) {
+        newstr += string
+    }
+    return newstr
 };
 
 // Do not edit below this line
