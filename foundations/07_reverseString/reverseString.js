f19580ed01df4f26b6e1189909443519c85b0a16
Jonah Yang <jonahyang2413@gmail.com>
2026-07-20 16:22:19 -0500

    complete 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    newstr = ''
+    for (let i = str.length - 1; i >= 0; i-- ) {
+        newstr += str.charAt(i)
+    }
+    return newstr
 };
 
 // Do not edit below this line
